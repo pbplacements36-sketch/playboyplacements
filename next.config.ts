cd08e3bebaca24804ca38136695c4e04b557d44a
Raaj Prajapati <mettorocks@gmail.com>
2025-09-12 12:46:18 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     unoptimized: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
